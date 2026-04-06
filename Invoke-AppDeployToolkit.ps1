dc6bc4a83c2285da6707c0af4533b5a8035b273c
yannDuchateau <98150537+yannDuchateau@users.noreply.github.com>
2026-04-06 11:01:08 +0200

    Update Invoke-AppDeployToolkit.ps1
    This Script show various ways to Silent Install Programms:
    
    Software that does not need setup: turned into . cab files
    Software that just need a reg file : Self Extracting archive (/S)
    Inno setup installer /VERYSILENT /SP-
    InstallShield made Setup files; Generally /S /V/qn
    VBSCript Made setups:  innovate ...

--- Invoke-AppDeployToolkit.ps1
+++ Invoke-AppDeployToolkit.ps1
@@ -28,6 +28,148 @@ $adtSession = @{
 };
 
 # detection
+[CmdletBinding()]
+param(
+[Parameter(Mandatory = $false)][ValidateSet('Install', 'Uninstall', 'Repair')][PSDefaultValue(Help = 'Install', Value = 'Install')][System.String]$DeploymentType,
+[Parameter(Mandatory = $false)][ValidateSet('Interactive', 'Silent', 'NonInteractive')][PSDefaultValue(Help = 'Interactive', Value = 'Interactive')][System.String]$DeployMode,
+[Parameter(Mandatory = $false)][System.Management.Automation.SwitchParameter]$AllowRebootPassThru,
+[Parameter(Mandatory = $false)][System.Management.Automation.SwitchParameter]$TerminalServerMode,
+[Parameter(Mandatory = $false)][System.Management.Automation.SwitchParameter]$DisableLogging
+)
+
+$adtSession = @{
+    AppVendor                   =  'software-options'
+    AppName                     =  'Software-options'
+    AppVersion                  =  '1.3.1'
+    AppArch                     =  'x64'
+    AppLang                     =  'en-us'
+    AppRevision                 =  '01'
+    AppSuccessExitCodes         =  @(0)
+    AppRebootExitCodes          =  @(1641, 3010)
+    AppScriptVersion            =  '1.0.0'
+    AppScriptDate               =  '2026-04-06'
+    AppScriptAuthor             =  'Yann Duchateau'
+    InstallName                 =  '' # doesn't need to be changed
+    InstallTitle                =  '' # doesn't need to be changed
+    DeployAppScriptFriendlyName =  $MyInvocation.MyCommand.Name
+    DeployAppScriptVersion      =  '4.0.6'
+    DeployAppScriptParameters   =  $PSBoundParameters
+};
+
+# detection
+# File Version  %programfiles%\Java\*\bin\Java.exe >= 8.u.471
+# how to : https://silentinstallhq.com/ #
+
+function Install-ADTDeployment {
+
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)";
+
+    $adtSession.InstallPhase = $adtSession.DeploymentType;
+
+    Start-ADTProcess `
+     -FilePath "Expand.exe" `
+     -ArgumentList " -r -F:* $($adtSession.dirFiles)\SMARTHDD.cab C:\";
+
+    Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\npp.8.8.8.Installer.x64.exe" `
+     -ArgumentList " /S /V/qn /L=1033 /S /NCRC";
+
+    Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\CABCompresser.exe" `
+     -ArgumentList " /S";
+
+     # Java never Updates in the same folder, and can keep old versions.
+    if ( ( Test-Path "$env:ProgramFiles\Java\*\bin\java.exe" ) ) { 
+    $JavaRoot = "$env:ProgramFiles\Java\";
+        if ( !$JavaRoot ) { 
+        Write-Host "Java not found!" -ForegroundColor Green 
+    Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\jre-8u471-windows-x64.exe" `
+     -ArgumentList " INSTALL_SILENT=1";
+         } 
+    else {  
+        Write-Host "Java found at $JavaRoot. Java is already Present." -ForegroundColor Green;
+         }
+    }
+
+    Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\PDFCreator-6_2_1-Setup.exe" `
+     -ArgumentList " /ForceInstall /VERYSILENT /LANG=French /COMPONENTS=program,ghostscript,languages\English";
+     
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\K-Lite_Codec_Pack_1920_Full.exe" `
+     -ArgumentList " /VERYSILENT /SP-";
+     
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\audacity-win-3.7.7-64bit.exe" `
+     -ArgumentList " /VERYSILENT /SP-";
+
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\FFmpeg_5.0.0_for_Audacity_on_Windows_x86_64.exe" `
+     -ArgumentList " /VERYSILENT /SP-";
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\spybotsd162.exe" `
+     -ArgumentList " /sp- /silent /norestart /lang=FRA /group=Spybot /components=!updatedl,!updatew95 /tasks=launchsdhelper";
+
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\spybotsd_includes_is.exe" `
+     -ArgumentList " /SP- /verysilent /SUPPRESSMSGBOXES";
+    
+    Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\3d_pinball_for_windows_-_space_cadet.exe" `
+     -ArgumentList "/S";
+
+     Start-ADTProcess `
+     -FilePath "RegEdit.exe" `
+     -ArgumentList "/S $($adtSession.dirFiles)\WinRar.reg";
+
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\WinRar-x64-401fr.exe" `
+     -ArgumentList "/S";
+     
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\winrar-x64-501.exe" `
+     -ArgumentList "/S";
+
+     Start-ADTProcess `
+     -FilePath "$($adtSession.dirFiles)\Opera_GX_128.0.5807.97_Setup_x64.exe" `
+     -ArgumentList " /silent /allusers=1 /launchopera=0 /setdefaultbrowser=0";
+
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)";
+
+}
+
+#region DONOTMODIFY
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue;
+Set-StrictMode -Version 1;
+# Initialization
+try {
+    $moduleName = if ([System.IO.File]::Exists("$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1")) {
+        Get-ChildItem -LiteralPath $PSScriptRoot\PSAppDeployToolkit -Recurse -File | Unblock-File -ErrorAction Ignore;
+        "$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1";
+    } else { 'PSAppDeployToolkit'; }
+    Import-Module -FullyQualifiedName @{ ModuleName = $moduleName; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.5' } -Force;
+    try {
+        $iadtParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation;
+        $adtSession = Open-ADTSession -SessionState $ExecutionContext.SessionState @adtSession @iadtParams -PassThru;
+    }
+    catch { Remove-Module -Name PSAppDeployToolkit* -Force; throw; }
+} catch { $Host.UI.WriteErrorLine((Out-String -InputObject $_ -Width ([System.Int32]::MaxValue))); exit 60008; }
+# Invocation
+try { 
+    Get-Item -Path $PSScriptRoot\PSAppDeployToolkit.* | & { process {
+        Get-ChildItem -LiteralPath $_.FullName -Recurse -File | Unblock-File -ErrorAction Ignore;
+        Import-Module -Name $_.FullName -Force;
+    } }
+    & "$($adtSession.DeploymentType)-ADTDeployment";
+    Close-ADTSession;
+} catch {
+    Write-ADTLogEntry -Message ($mainErrorMessage = Resolve-ADTErrorRecord -ErrorRecord $_) -Severity 3;
+    Show-ADTDialogBox -Text $mainErrorMessage -Icon Stop | Out-Null;
+    Close-ADTSession -ExitCode 60001;
+} finally { Remove-Module -Name PSAppDeployToolkit* -Force; }
+#endregion DONOTMODIFY
 # File Version  %programfiles%\Java\jre1.8.0_461\bin\Java.exe >= 8.u.471
 # how to : https://silentinstallhq.com/ #
 
